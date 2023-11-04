8c0bc7049884c6203d671df293ca6a658036f715
CharlsBabu <charlespbabu@gmail.com>
2023-11-04 14:27:35 -0500

    eva init

--- cache_model.py
+++ cache_model.py
@@ -1,4 +1,5 @@
 from collections import deque
+import numpy as np
 
 class CacheLine:
     def __init__(self, addr):
@@ -8,17 +9,30 @@ class CacheLine:
 class HexDeque(deque):
     def __str__(self):
         return "[" + ", ".join(hex(item) for item in self) + "]"
-    
-#VERBOSE > 0 : Shows each cache line access with hit and miss info.
-VERBOSE = 0
 
-CACHE_SIZE = 4
+#VERBOSE > 0 : Shows each cache line access with hit and miss info.
+VERBOSE = 1
+
+CACHE_SIZE = 6
 cache = []
 fifo_queue = HexDeque()
 
-# Initialize hit and miss counters for each age
+# Initialize hit and miss counters for each age 
 hit_counters = {}
 miss_counters = {}
+eviction_counters = {}
+
+# Initialize cumulative hit, miss, lifetime counters
+lifetimes_a = np.array([])
+hits_a = np.array([])
+miss_a = np.array([])
+evictions_a = np.array([])
+
+hits_gt_a = np.array([])
+evictions_gt_a = np.array([])
+lifetimes_gt_a = np.array([])
+expected_lifetimes_a = np.array([])
+EVA = np.array([])
 
 # Arrays A and B information
 array_a = {"start_addr": 0x100, "length": 2}
@@ -50,7 +64,7 @@ def cache_access(addr):
         print(f"Accessing:  0x{addr:04X}")
     index = find_cache_line(addr)
     # Increment the ages of all cache lines
-    increment_cache_ages()
+    increment_cache_ages() #TODO: At beginning or end??
 
     if index is not None:
         # Cache hit
@@ -67,9 +81,9 @@ def cache_access(addr):
         else:
             cache_line = CacheLine(addr)
             cache.append(cache_line)
-            cache[-1].age = 0  # Set the age of the newly added cache line to 0
-            #NOT UPDATING MISS COUNTER IN COMPULSORY MISS
-            #update_miss_counters(cache_line.age)
+            #cache[-1].age = 0  # Implicit # Set the age of the newly added cache line to 0
+            #UPDATING MISS COUNTER IN COMPULSORY MISS
+            update_miss_counters(cache_line.age)
             if VERBOSE>0:
                 print(f"Cache miss: Address 0x{addr:04X} added to cache")
 
@@ -103,10 +117,15 @@ def update_hit_counters(age):
         hit_counters[age] += 1
 
 def update_miss_counters(age):
-    if age not in miss_counters:
+    if age not in miss_counters: #miss counters by age is actually evictions
         miss_counters[age] = 1
     else:
         miss_counters[age] += 1
+    
+    if age not in eviction_counters:
+        eviction_counters[age] = 1
+    else:
+        eviction_counters[age] += 1
 
 def print_cache_contents():
     print("Current cache contents:")
@@ -114,17 +133,56 @@ def print_cache_contents():
     for i, cache_line in enumerate(cache):
         print(f"Cache Line {i}: Address 0x{cache_line.addr:04X}, Age {cache_line.age}")
 
-# Print hit and miss counters for each age
-def print_hit_miss_counters():
+def upscan(arr):
+    rev = arr[::-1]
+    tot = np.cumsum(rev)
+    tot_rev = tot[::-1]
+    res = np.append(tot_rev[1:], 0)
+    return res    
+    
+def update_statistics():
+    global hits_a
+    global miss_a
+    global evictions_a
+    global lifetimes_a
+    global hits_gt_a
+    global evictions_gt_a
+    global lifetimes_gt_a
+    global expected_lifetimes_a
+    
+    max_age = max(max(hit_counters.keys(), default=0), 
+                  max(miss_counters.keys(), default=0), 
+                  max(eviction_counters.keys(), default=0))
+    
+    for age in range(max_age,-1,-1):
+        hits_a = np.insert(hits_a, 0, hit_counters.get(age, 0))
+        miss_a = np.insert(miss_a, 0, miss_counters.get(age, 0))
+        evictions_a = np.insert(evictions_a, 0, eviction_counters.get(age, 0))
+                     
+    lifetimes_a = hits_a + evictions_a
+    hits_gt_a = upscan(hits_a)
+    evictions_gt_a = upscan(evictions_a)
+    lifetimes_gt_a = upscan(lifetimes_a) #hits_gt_a + evictions_gt_a
+    expected_lifetimes_a = np.cumsum(lifetimes_gt_a[::-1])[::-1]
+
+def print_hit_miss_counters():  
     print("Hit Counters:")
     print("*****")
-    max_age = max(max(hit_counters.keys(), default=0), max(miss_counters.keys(), default=0))
     print("Hits and Misses by Age:")
-    for age in range(max_age + 1):
-        hit_count = hit_counters.get(age, 0)
-        miss_count = miss_counters.get(age, 0)
-        print(f"Age {age}: Hits - {hit_count}, Misses - {miss_count}")
+    for age in range(len(lifetimes_a)):  
+        print(f"Age {age}: Hits - {hits_a[age]}, Evictions - {evictions_a[age]}, \
+Lifetimes - {lifetimes_a[age]}, Expected Lifetimes - {expected_lifetimes_a[age]}")
+        
+    print("Hits > a", hits_gt_a)
+    print("Evictions > a", evictions_gt_a) 
+    print("Lifetime > a", lifetimes_gt_a)
     print("\n")
+    
+# Compute EVA
+#def compute_eva():
+#    max_age = max(lifetime_a, default=0)
+#    expected_lifetimes = upscan(lifetimes_gt_a)
+    
 
 # Simulate cache accesses using the alternating access pattern
 for _ in range(NUM_ACCESS):
@@ -136,4 +194,5 @@ for _ in range(NUM_ACCESS):
 
 # Print the current cache contents using the new function
 print_cache_contents()
+update_statistics()
 print_hit_miss_counters()
